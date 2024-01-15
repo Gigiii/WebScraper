c1d45e9b47b06731b6eeca7a8c8f29743b278bb1
George Ugulava <giorgi.ugulava@gau.edu.ge>
2024-01-15 19:05:20 +0000

    Added explanation comments to run_web_application.py

--- webscraper_flaskr_project/run_web_application.py
+++ webscraper_flaskr_project/run_web_application.py
@@ -2,11 +2,14 @@ from news_scraper.main import create_app
 import webbrowser
 from threading import Timer
 
-
+#Opens the localhost in a webbrowser
 def open_browser():
     webbrowser.open_new("http://127.0.0.1:5000")
 
+#Create the application from main, turn off debugging
 app = create_app()
 app.debug = False
+
+#Start the localhost browser in 0.5 seconds and run the app to be hosted
 Timer(0.5, open_browser).start()
 app.run(host='0.0.0.0', port=5000)
